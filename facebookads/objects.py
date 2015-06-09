62e1fd52a6e4842f052a6301c75925a4c8767c4c
Cassio D'Angelo Greco <cassiogreco@fb.com>
2015-06-09 06:50:25 -0700

    Changed AdPreview to AdCreativePreview in AdCreative get_ad_preview method
    Summary:
    The AdCreative get_ad_preview method in the Python Ads Sdk currently iterates over AdPreview objects, whose endpoint is 'generatepreviews', instead of AdCreativePreview, whose endpoint is 'previews'
    
    This is inconsistent with the PHP Ads Sdk, which iterates over AdPreview but with the 'previews' endpoint (the AdCreativePreview object does not exist in the PHP Ads SDK), and with https://developers.intern.facebook.com/docs/marketing-api/generatepreview/v2.3#examples
    
    The first examples shows a call to the endpoint 'previews' which currently isn't supported by the Python Ads Sdk.
    
    This is a blocking task for t6861580
    
    Test Plan: create an AdCreative object and call get_ad_preview. You will currently get an error that the endpoint doesn't exist. Change AdPreview to AdCreativePreview and the method will now work.

--- facebookads/objects.py
+++ facebookads/objects.py
@@ -1339,8 +1339,10 @@ class AdCreative(AbstractCrudObject):
         return 'adcreatives'
 
     def get_ad_preview(self, fields=None, params=None):
-        """Returns iterator over AdPreview's associated with this creative."""
-        return self.iterate_edge(AdPreview, fields, params)
+        """
+        Returns iterator over AdCreativePreview's associated with this creative.
+        """
+        return self.iterate_edge(AdCreativePreview, fields, params)
 
 
 class AdImage(CannotUpdate, AbstractCrudObject):
