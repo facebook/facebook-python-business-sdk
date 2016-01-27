2915ce36b01a6dac17f9e74113d227f759722865
Rituparna Mukherjee <ritu@fb.com>
2016-01-27 10:54:03 -0800

    Adding logic to doc_runner to act based on different exit codes
    Summary: Adding logic in doc_runner.py to consider custom exit codes while executing docsmith tests
    
    Test Plan: Sandcastle

--- facebookads/exceptions.py
+++ facebookads/exceptions.py
@@ -62,6 +62,7 @@ class FacebookRequestError(FacebookError):
         self._api_error_type = None
         self._api_error_message = None
         self._api_blame_field_specs = None
+        self._api_transient_error = False
 
         if self._body and 'error' in self._body:
             self._error = self._body['error']
@@ -69,6 +70,8 @@ class FacebookRequestError(FacebookError):
                 self._api_error_message = self._error['message']
             if 'code' in self._error:
                 self._api_error_code = self._error['code']
+            if 'is_transient' in self._error:
+                self._api_transient_error = self._error['is_transient']
             if 'error_subcode' in self._error:
                 self._api_error_subcode = self._error['error_subcode']
             if 'type' in self._error:
@@ -127,6 +130,12 @@ class FacebookRequestError(FacebookError):
     def api_blame_field_specs(self):
         return self._api_blame_field_specs
 
+    def api_transient_error(self):
+        return self._api_transient_error
+
+    def get_message(self):
+        return self._message
+
 
 class FacebookBadObjectError(FacebookError):
     """Raised when a guarantee about the object validity fails."""
@@ -136,3 +145,12 @@ class FacebookBadObjectError(FacebookError):
 class FacebookUnavailablePropertyException(FacebookError):
     """Raised when an object's property or method is not available."""
     pass
+
+
+class DocsmithSkipTestError(Exception):
+    """Raised when a docsmith test is skipped."""
+    def __init__(self, message):
+        self._message = message
+
+    def get_skip_error_msg(self):
+        return self._message
