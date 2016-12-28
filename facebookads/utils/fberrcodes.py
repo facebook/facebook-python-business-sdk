6b88467da630f4ca4b1f7c9a248829c31aeb835f
Pavel Kirichenko <pasha-r@standalone.name>
2016-12-28 14:16:30 +0100

    added 200 error code

--- facebookads/utils/fberrcodes.py
+++ facebookads/utils/fberrcodes.py
@@ -1,12 +1,9 @@
-from __future__ import print_function, unicode_literals, absolute_import
-
-__author__ = 'pasha-r'
-
 class FacebookErrorCodes(object):
     unknown = 1
     temporary = 2
     rate_limit = 17
     unsupported_request = 100
+    permissions_error = 200
     too_much_data = -3
     not_on_white_list = 1487654
     report_cannot_be_accessed = 2601
