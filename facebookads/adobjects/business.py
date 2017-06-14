e63866a062c32bbd589ccd12a2adf614460cf5e1
abetomo <abe@enzou.tokyo>
2017-06-14 18:07:46 +0900

    Add `get_ad_accounts` to `Business`

--- facebookads/adobjects/business.py
+++ facebookads/adobjects/business.py
@@ -160,6 +160,34 @@ class Business(
             self.assure_call()
             return request.execute()
 
+    def get_ad_accounts(self, fields=None, params=None, batch=None, pending=False):
+        from facebookads.adobjects.adaccount import AdAccount
+        param_types = {
+        }
+        enums = {
+        }
+        request = FacebookRequest(
+            node_id=self['id'],
+            method='GET',
+            endpoint='/adaccounts',
+            api=self._api,
+            param_checker=TypeChecker(param_types, enums),
+            target_class=AdAccount,
+            api_type='EDGE',
+            response_parser=ObjectParser(target_class=AdAccount),
+        )
+        request.add_params(params)
+        request.add_fields(fields)
+
+        if batch is not None:
+            request.add_to_batch(batch)
+            return request
+        elif pending:
+            return request
+        else:
+            self.assure_call()
+            return request.execute()
+
     def create_ad_accounts(self, fields=None, params=None, batch=None, pending=False):
         from facebookads.adobjects.adaccount import AdAccount
         param_types = {
