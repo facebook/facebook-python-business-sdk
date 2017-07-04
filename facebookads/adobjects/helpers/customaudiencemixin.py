715376a0c90b33e3cf3837b6f2072f267deb8752
Paul Booth <thephantompaulbooth@gmail.com>
2017-07-03 17:00:49 -0700

    Allow specifying session in add_users and remove_users. (#280)
    Including session allows for larger uploads of users, and allows
     tracking the session progress and state for upload completion.
    Documentation for session parameter available at
     https://developers.facebook.com/docs/marketing-api/reference/custom-audience/users

--- facebookads/adobjects/helpers/customaudiencemixin.py
+++ facebookads/adobjects/helpers/customaudiencemixin.py
@@ -56,7 +56,8 @@ class CustomAudienceMixin:
                       users,
                       is_raw=False,
                       app_ids=None,
-                      pre_hashed=None):
+                      pre_hashed=None,
+                      session=None):
         hashed_users = []
         if schema in (cls.Schema.phone_hash,
                       cls.Schema.email_hash,
@@ -122,9 +123,12 @@ class CustomAudienceMixin:
         if app_ids:
             payload['app_ids'] = app_ids
 
-        return {
+        params = {
             'payload': payload,
         }
+        if session:
+            params['session'] = session
+        return params
 
     @classmethod
     def normalize_key(cls, key_name, key_value=None):
@@ -180,7 +184,8 @@ class CustomAudienceMixin:
                   users,
                   is_raw=False,
                   app_ids=None,
-                  pre_hashed=None):
+                  pre_hashed=None,
+                  session=None):
         """Adds users to this CustomAudience.
 
         Args:
@@ -199,6 +204,7 @@ class CustomAudienceMixin:
                  is_raw,
                  app_ids,
                  pre_hashed,
+                 session,
             ),
         )
 
@@ -207,7 +213,8 @@ class CustomAudienceMixin:
                      users,
                      is_raw=False,
                      app_ids=None,
-                     pre_hashed=None):
+                     pre_hashed=None,
+                     session=None):
         """Deletes users from this CustomAudience.
 
         Args:
@@ -227,6 +234,7 @@ class CustomAudienceMixin:
                 is_raw,
                 app_ids,
                 pre_hashed,
+                session,
             ),
         )
 
