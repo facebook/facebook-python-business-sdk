c67bab840669e4983da9351af48eab4238614fc2
Pavel Kirichenko <pasha-r@standalone.name>
2018-08-22 15:37:41 +0200

    added error subcodes class

--- facebookads/utils/fberrcodes.py
+++ facebookads/utils/fberrcodes.py
@@ -1,12 +1,17 @@
-from __future__ import print_function, unicode_literals, absolute_import
-
-__author__ = 'pasha-r'
-
 class FacebookErrorCodes(object):
     unknown = 1
     temporary = 2
+    object_not_owned_by_business = 10
     rate_limit = 17
+    too_many_requests = 613
     unsupported_request = 100
+    permissions_error = 200
     too_much_data = -3
     not_on_white_list = 1487654
     report_cannot_be_accessed = 2601
+    service_error = 2650
+    source_audience_too_small = 2654
+
+
+class FacebookErrorSubCodes(object):
+    already_archived = 1487862
