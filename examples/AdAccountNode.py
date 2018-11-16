9fac79e7abc290ac1f5fbd6f3daeb96a9ce65813
Jing Ping <jingping@fb.com>
2018-11-16 15:35:36 -0800

    Auto-generated python SDK code update
    Summary: ignore-conflict-markers
    
    Reviewed By: jingping2015
    
    Differential Revision: D13109780
    
    fbshipit-source-id: 97d31818

--- examples/AdAccountNode.py
+++ examples/AdAccountNode.py
@@ -18,8 +18,8 @@
 # FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 # DEALINGS IN THE SOFTWARE.
 
-from facebookads.adobjects.adaccount import AdAccount
-from facebookads.api import FacebookAdsApi
+from facebook_business.adobjects.adaccount import AdAccount
+from facebook_business.api import FacebookAdsApi
 
 access_token = '<ACCESS_TOKEN>'
 app_secret = '<APP_SECRET>'
