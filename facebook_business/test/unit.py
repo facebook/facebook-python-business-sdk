34db561b8898c1b3f0a1113a01224ab6d0f7de2c
Jing Ping <jingping@fb.com>
2019-07-11 12:05:56 -0700

    Fix test

--- facebook_business/test/unit.py
+++ facebook_business/test/unit.py
@@ -43,8 +43,7 @@ from facebook_business.adobjects import (abstractcrudobject,
                                    adaccount,
                                    adcreative,
                                    customaudience,
-                                   productcatalog,
-                                   adaccountreachestimate)
+                                   productcatalog)
 from facebook_business.utils import version
 
 
@@ -200,33 +199,22 @@ class EdgeIteratorTestCase(unittest.TestCase):
     def test_builds_from_object_with_data_key(self):
         """
         Sometimes the response returns a single JSON object - with a "data".
-        For instance with adaccountreachestimate. This asserts that we successfully
+        For instance with adcreative. This asserts that we successfully
         build the object that is in "data" key.
         """
         response = {
             "data": {
-                "estimate_ready": True,
-                "bid_estimations": [{
-                    "cpa_min": 63,
-                    "cpa_median": 116,
-                    "cpm_max": 331,
-                    "cpc_max": 48,
-                    "cpc_median": 35,
-                    "cpc_min": 17,
-                    "cpm_min": 39,
-                    "cpm_median": 212,
-                    "unsupported": False,
-                    "location": 3,
-                    "cpa_max": 163}],
-                "users": 7600000
+                "name": "test name",
+                "status": "ACTIVE",
+                "account_id": 'act_345'
             }
         }
         ei = api.Cursor(
             ad.Ad('123'),
-            adaccountreachestimate.AdAccountReachEstimate,
+            adcreative.AdCreative,
         )
         obj = ei.build_objects_from_response(response)
-        assert len(obj) == 1 and obj[0]['users'] == 7600000
+        assert len(obj) == 1 and obj[0]['account_id'] == 'act_345'
 
 class AbstractCrudObjectTestCase(unittest.TestCase):
     def test_all_aco_has_id_field(self):
