fec5f45a50cfe98207178f4644a504f1e0afa9f1
Serhii Maltsev <alternativshik@gmail.com>
2020-05-14 14:36:42 +0300

    add transaction details endpoint

--- facebook_business/adobjects/adaccount.py
+++ facebook_business/adobjects/adaccount.py
@@ -3839,6 +3839,39 @@ class AdAccount(
         else:
             self.assure_call()
             return request.execute()
+   
+    def get_transaction_details(self, fields=None, params=None, batch=None, success=None, failure=None, pending=False):
+        from facebook_business.utils import api_utils
+        if batch is None and (success is not None or failure is not None):
+            api_utils.warning('`success` and `failure` callback only work for batch call.')
+        from facebook_business.adobjects.transaction import Transaction
+        # param_types = {
+        #     'time_start': 'int',
+        #     'time_stop': 'int',
+        # }
+        enums = {
+        }
+        request = FacebookRequest(
+            node_id=self['id'],
+            method='GET',
+            endpoint='/transaction_details',
+            api=self._api,
+            # param_checker=TypeChecker(param_types, enums),
+            target_class=Transaction,
+            api_type='EDGE',
+            # response_parser=ObjectParser(target_class=Transaction, api=self._api),
+        )
+        request.add_params(params)
+        request.add_fields(fields)
+
+        if batch is not None:
+            request.add_to_batch(batch, success=success, failure=failure)
+            return request
+        elif pending:
+            return request
+        else:
+            self.assure_call()
+            return request.execute()
     
     def get_users(self, fields=None, params=None, batch=None, success=None, failure=None, pending=False):
         from facebook_business.utils import api_utils
