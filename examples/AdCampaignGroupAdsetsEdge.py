070ebf3f90b1b07434c2d75c46cb02cbbf577df1
yeetnbeet <segalcsam@gmail.com>
2023-08-02 17:18:04 -0600

    Fixed Some Code Typos

--- examples/AdCampaignGroupAdsetsEdge.py
+++ examples/AdCampaignGroupAdsetsEdge.py
@@ -27,7 +27,7 @@ access_token = '<ACCESS_TOKEN>'
 app_secret = '<APP_SECRET>'
 app_id = '<APP_ID>'
 id = '<AD_CAMPAIGN_ID>'
-FacebookAdsApi.init(access_token=access_token)
+FacebookAdsApi.init(access_token=access_token, app_secret=app_secret, app_id=app_id)
 
 fields = [
   'name',
@@ -36,9 +36,9 @@ fields = [
   'daily_budget',
   'lifetime_budget',
 ]
-params = {
-}
-print Campaign(id).get_ad_sets(
-  fields=fields,
-  params=params,
-)
+
+campaign = Campaign(id)
+ad_sets = campaign.get_ad_sets(fields=fields)
+
+for ad_set in ad_sets:
+    print(ad_set)
