6ae938a3cc6591e027b7758725955ad3eff035a9
Li Li (AXP) <lil@meta.com>
2024-09-18 13:41:03 -0700

    Bump SDK Versions By 0.0.1
    Summary: as titled
    
    Reviewed By: yoongyj, stcheng
    
    Differential Revision: D62960011
    
    fbshipit-source-id: cdccbc786e857b514ebbe030cf91628f4ed1ee1f

--- facebook_business/__init__.py
+++ facebook_business/__init__.py
@@ -7,5 +7,5 @@
 from facebook_business.session import FacebookSession
 from facebook_business.api import FacebookAdsApi
 
-__version__ = '20.0.2'
+__version__ = '20.0.3'
 __all__ = ['session', 'api']
