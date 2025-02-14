4148fc650f1c45df1cfd677a3a8d9033fbb07311
Satwika Reddy Maddi <satwika@meta.com>
2025-02-13 17:17:01 -0800

    Bump python version by 0.0.2
    Summary: this is to address change in D69626558
    
    Reviewed By: stcheng, liliarizona
    
    Differential Revision: D69631502
    
    fbshipit-source-id: 49359b758b2f6443b899350641e6b7e1b8f71319

--- facebook_business/__init__.py
+++ facebook_business/__init__.py
@@ -7,5 +7,5 @@
 from facebook_business.session import FacebookSession
 from facebook_business.api import FacebookAdsApi
 
-__version__ = '22.0.0'
+__version__ = '22.0.2'
 __all__ = ['session', 'api']
--- facebook_business/apiconfig.py
+++ facebook_business/apiconfig.py
@@ -6,6 +6,6 @@
 
 ads_api_config = {
   'API_VERSION': 'v22.0',
-  'SDK_VERSION': 'v22.0.0',
+  'SDK_VERSION': 'v22.0.2',
   'STRICT_MODE': False
 }
